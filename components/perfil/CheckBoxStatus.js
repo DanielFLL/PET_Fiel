0d9490b1d655d78c8f47c780ab49c8f9b13192f4
DanielFLL <daniel-dfl@hotmail.com>
2024-03-12 09:53:35 -0300

    check box update 1

--- components/perfil/CheckBoxStatus.js
+++ components/perfil/CheckBoxStatus.js
@@ -1,49 +1,78 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { Checkbox } from 'react-native-paper';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Checkbox } from "react-native-paper";
 
 const CheBoxStatus = () => {
-    const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = useState(false);
+  const [opcao, setOpcao] = useState("");
 
-    useEffect(() => {
-        const enviarDadosParaBD = () => {
-          fetch('http://localhost:8080/status', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              opcaoMarcada: checked,
-            }),
-          })
-          .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao enviar: ' + response.statusText);
-            }
-            return response.json();
-          })
-            .then(data => {
-              // Lógica adicional após o envio, se necessário
-            })
-            .catch(error => {
-              console.error('Erro ao enviar:', error.message);
-            });
-        };
-    
-        if (checked) {
-          enviarDadosParaBD();
+  useEffect(() => {
+    const enviarDadosParaBD = () => {
+      fetch("http://localhost:8080/status", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          opcaoMarcada: checked,
+        }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Erro ao enviar: " + response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Lógica adicional após o envio, se necessário
+        })
+        .catch((error) => {
+          console.error("Erro ao enviar:", error.message);
+        });
+    };
+
+    if (checked) {
+      enviarDadosParaBD();
+    }
+  }, [checked]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/status", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        opcaoMarcada: checked,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro ao enviar: " + response.statusText);
         }
-      }, [checked]);
+        return response.json();
+      })
+      .then((data) => {
+        setOpcao(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao enviar:", error.message);
+      });
+  }, []);
 
-    return (
-        // Adicionar aqui o Status com Text ou algo similar!!! Requisitar do BD.
-        <Checkbox
-            status={checked ? 'checked' : 'unchecked'}
-            onPress={() => {
-                setChecked(!checked);
-            }}
-        />
-    );
+  return (
+    // Adicionar aqui o Status com Text ou algo similar!!! Requisitar do BD.
+    <View>
+      <Text variant="headlineMedium">{opcao}</Text>
+      <Checkbox
+        status={checked ? "checked" : "unchecked"}
+        onPress={() => {
+          setChecked(!checked);
+        }}
+      />
+    </View>
+  );
 };
 
 export default CheBoxStatus;
